3b3afbe64938c3a574d9d20d576a964fd5406f1b
Ranjith <ranjithramanathan47@gmail.com>
2026-05-29 14:31:29 +0530

    Type Casting program

---
+++ Upskilling/Core Java/TypeCasting.java
@@ -0,0 +1,13 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        double X = 47.16;
+        int Y = (int) X;
+        int A = 25;
+        double B = (double) A;
+        System.out.println("Value of X: " + X);
+        System.out.println("Value of Y: " + Y);
+        System.out.println("Value of A: " + A);
+        System.out.println("Value of B: " + B);
+    }
+
+}
