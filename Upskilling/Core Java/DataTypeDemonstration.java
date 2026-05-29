69585aa9fae2ff2a3b6293840b89572bb3aa3cc3
Ranjith <ranjithramanathan47@gmail.com>
2026-05-29 14:25:54 +0530

    Data Type Demonstration program

---
+++ Upskilling/Core Java/DataTypeDemonstration.java
@@ -0,0 +1,15 @@
+public class DataTypeDemonstration {
+    public static void main(String[] args) {
+        int a = 5;
+        float b = 4.6f;
+        double c = 27.88;
+        boolean d = true;
+        char e = 'R';
+
+        System.out.println("Value of a: " + a);
+        System.out.println("Value of b: " + b);
+        System.out.println("Value of c: " + c);
+        System.out.println("Value of d: " + d);
+        System.out.println("Value of e: " + e);
+    }
+}
