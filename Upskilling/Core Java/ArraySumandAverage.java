f84ac8c46a39d57f3d5202445b070c9a7f27fa0d
Ranjith <ranjithramanathan47@gmail.com>
2026-05-29 19:58:55 +0530

    Array sum and average program

--- Upskilling/Core Java/ArraySumandAverage.java
+++ Upskilling/Core Java/ArraySumandAverage.java
@@ -1,3 +1,19 @@
-public class ArraySumandAverage {
+import java.util.Scanner;
 
+public class ArraySumandAverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        System.out.print("Enter the elements: ");
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+        System.out.println("Sum : " + sum);
+        double average = (double) sum / N;
+        System.out.println("Average : " + average);
+    }
 }
--- Upskilling/Core Java/RecursiveFibonacci.java
+++ Upskilling/Core Java/RecursiveFibonacci.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class RecursiveFibonacci {
     public static int fib(int N) {
