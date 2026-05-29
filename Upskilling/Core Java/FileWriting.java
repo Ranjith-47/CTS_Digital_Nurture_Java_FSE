f6ddf20ec1a91b206803271708788b1e3bd2be90
Ranjith <ranjithramanathan47@gmail.com>
2026-05-29 22:40:56 +0530

    File writing program

---
+++ Upskilling/Core Java/FileWriting.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class FileWriting {
+    public static void main(String[] args) {
+        try {
+            FileWriter writer = new FileWriter("sample.txt");
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter text: ");
+            String text = sc.nextLine();
+            writer.write(text);
+            writer.close();
+            System.out.println("File written successfully");
+        } catch (IOException e) {
+            System.out.println("Error while writing to file ");
+            e.printStackTrace();
+        }
+    }
+}
