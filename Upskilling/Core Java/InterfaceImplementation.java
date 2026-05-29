ddc4b9adbde5ba5be44b6f60c32c93b5d5d322c9
Ranjith <ranjithramanathan47@gmail.com>
2026-05-29 21:20:30 +0530

    Interface program

---
+++ Upskilling/Core Java/InterfaceImplementation.java
@@ -0,0 +1,26 @@
+interface Playable {
+    void play();
+}
+
+class Guitar implements Playable {
+    @Override
+    public void play() {
+        System.out.println("Guitar");
+    }
+}
+
+class Piano implements Playable {
+    @Override
+    public void play() {
+        System.out.println("Piano");
+    }
+}
+
+public class InterfaceImplementation {
+    public static void main(String[] args) {
+        Playable guitar = new Guitar();
+        Playable piano = new Piano();
+        guitar.play();
+        piano.play();
+    }
+}
